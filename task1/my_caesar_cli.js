f637fce60c75aecb66c5fcdd0c677539ad76aefb
tanyak1601 <tanyakk1601@gmail.com>
2020-03-26 22:11:50 +0300

    feat: add task1

---
+++ task1/my_caesar_cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const { pipeline } = require('stream');
+const { Command } = require('commander');
+const caesarCipher = require('./utils/caesarCipher');
+const program = new Command();
+
+process.on('exit', code => {
+  console.log(`Exit with status code: ${code}`);
+});
+
+program
+  .requiredOption('-s, --shift [value]', 'a shift')
+  .option('-i, --input [value]', 'an input file')
+  .option('-o, --output [value]', 'an output file')
+  .requiredOption('-a, --action [value]', 'an action encode/decode');
+
+program.parse(process.argv);
+
+const opts = program.opts();
+
+const readable = opts.input ? fs.createReadStream(opts.input) : process.stdin;
+const writable = opts.output
+  ? fs.createWriteStream(opts.output)
+  : process.stdout;
+
+pipeline(readable, caesarCipher(opts), writable, err => {
+  if (err) {
+    console.error('Pipeline failed.', err.message);
+  } else {
+    console.log('Pipeline succeeded.');
+  }
+});
